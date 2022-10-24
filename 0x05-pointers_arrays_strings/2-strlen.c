53a5c6df4277e7d33fa7d76fa86bae588398d212
James Aworo <james001.awo@gmail.com>
2022-10-24 14:35:30 -0700

    add 2-strlen tasl

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input
+ * Return: return int
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
