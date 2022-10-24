7ab068f1c41ede4b8bbfd51606811fc5a0df778d
James Aworo <james001.awo@gmail.com>
2022-10-24 14:25:13 -0700

    add task file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
  * @n: input
  * Return: nothing
  */
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first int
+ * @b: second int
+ * Return: nothing
+ */
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
+#endif /*MAIN_H*/
