ce61e3bf6a819ee8ebba9cac9abec61d8b89a49f
James Aworo <james001.awo@gmail.com>
2022-10-24 14:05:45 -0700

    fix betty warnings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value to 98.
+ * reset_to_98 - takes a pointer and updates the value to 98.
  * @n: input to update
  * Return: no return
  */
