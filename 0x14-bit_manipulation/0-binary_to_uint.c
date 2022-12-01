eb03ba3960a85f8b9ea21d42abf5dbef8cd96f75
James Aworo <james001.awo@gmail.com>
2022-12-01 10:14:02 -0800

    fix betty warning

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,29 +7,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-	int len = 0, i;
-	unsigned int sum = 0;
+	int len = 0, pow = 1;
+	unsigned int sum = 0, error = 0;
 
 	if (b == NULL)
-		return (sum);
+		return (error);
 
-	/* find string length */
 	while (b[len] != '\0')
 		len++;
 	len -= 1;
 
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	while (len >= 0)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		if ((b[len] != '0') && (b[len] != '1'))
+			return (error);
 
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
+		if (b[len] == '1')
+			sum += pow;
+
+		pow *= 2;
 		len--;
 	}
 
